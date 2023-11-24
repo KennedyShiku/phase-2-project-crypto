67645020ab259758cab0fd72f1b2c96e3fe83e19
KennedyShiku <kenrichshiku@gmail.com>
2023-11-24 18:09:56 +0300

    Added alt to images

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -7,7 +7,7 @@ const Blog = () => {
               <h3>Binance CEO</h3>
               <h4>Changpeng Zhao pleads guilty</h4>
               <div className="image-container">
-                <img src="https://c.ndtvimg.com/2023-03/ltpnt4a8_binances-changpeng-zhao_625x300_28_March_23.jpg" />
+                <img src="https://c.ndtvimg.com/2023-03/ltpnt4a8_binances-changpeng-zhao_625x300_28_March_23.jpg" alt="blog1"/>
               </div>
               <p>Binance founder pleads guilty to money laundering charges. Largest global crypto exchange agrees to pay $4.3 billion in fines.</p>
             </div>
@@ -15,7 +15,7 @@ const Blog = () => {
               <h3>Binance</h3>
               <h4>What is Binance?</h4>
               <div className="image-container">
-                <img src="https://media.cnn.com/api/v1/images/stellar/prod/gettyimages-1660785093.jpg?c=16x9&q=h_720,w_1280,c_fill" />
+                <img src="https://media.cnn.com/api/v1/images/stellar/prod/gettyimages-1660785093.jpg?c=16x9&q=h_720,w_1280,c_fill" alt="blog2"/>
               </div>
               <p>What is Binance? Why is it in so much trouble? ANd what does this mean for crypto?</p>
             </div>
@@ -23,7 +23,7 @@ const Blog = () => {
               <h3>Crypto</h3>
               <h4>Crypto Prices Crash</h4>
               <div className="image-container">
-                <img src="https://cdn.vox-cdn.com/thumbor/qOT_KaIjJwsloctLQGzr2lhNq9s=/0x0:2032x1475/1200x800/filters:focal(854x576:1178x900)/cdn.vox-cdn.com/uploads/chorus_image/image/71665176/GettyImages_1338902668.0.jpg" />
+                <img src="https://cdn.vox-cdn.com/thumbor/qOT_KaIjJwsloctLQGzr2lhNq9s=/0x0:2032x1475/1200x800/filters:focal(854x576:1178x900)/cdn.vox-cdn.com/uploads/chorus_image/image/71665176/GettyImages_1338902668.0.jpg" alt="blog3"/>
               </div>
               <p>Crypto prices fall as Binance chief pleads guilty to DOJ charges!</p>
             </div>
